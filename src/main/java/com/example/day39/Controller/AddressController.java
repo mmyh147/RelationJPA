ff72703be22ac1baf87c6a287aeabdbf554045ac
mmyh147 <mmyh-1000000@hotmail.com>
2024-04-03 16:33:46 +0300

    add logs

--- src/main/java/com/example/day39/Controller/AddressController.java
+++ src/main/java/com/example/day39/Controller/AddressController.java
@@ -4,6 +4,8 @@ import com.example.day39.DTO.AddressDTO;
 import com.example.day39.Service.AddressService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,21 +17,26 @@ public class AddressController {
 
 
     private final AddressService addressService;
+    Logger logger = LoggerFactory.getLogger(TeacherController.class);
 
 
     @GetMapping("get")
-    public ResponseEntity getAllProfile(){
+    public ResponseEntity getAllAddress(){
+        logger.info("get all address");
+
         return ResponseEntity.ok(addressService.getAll());
     }
 
     @PostMapping("add")
     public ResponseEntity addAddress(@RequestBody @Valid AddressDTO addressDTO){
+        logger.info("add address");
         addressService.add(addressDTO);
         return ResponseEntity.ok("address added");
     }
 
     @PutMapping("update")
     public ResponseEntity updateAddress(@RequestBody @Valid AddressDTO addressDTO){
+        logger.info("update address");
 
         addressService.update(addressDTO);
         return ResponseEntity.status(200).body("address updated");
@@ -40,6 +47,7 @@ public class AddressController {
 
     @DeleteMapping("delete/{id}")
     public ResponseEntity deleteAddress(@PathVariable Integer id){
+        logger.info("delete address");
 
         addressService.delete(id);
         return ResponseEntity.status(200).body("address deleted");
--- src/main/java/com/example/day39/Controller/TeacherController.java
+++ src/main/java/com/example/day39/Controller/TeacherController.java
@@ -5,6 +5,8 @@ import com.example.day39.Repository.TeacherRepository;
 import com.example.day39.Service.TeacherService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,21 +19,26 @@ public class TeacherController {
 
 
     private final TeacherService teacherService;
+    Logger logger = LoggerFactory.getLogger(TeacherController.class);
 
     @GetMapping("get")
     public ResponseEntity getAllTeacher(){
+        logger.info("get all teacher");
+
         return ResponseEntity.status(200).body(teacherService.getAll());
     }
 
     @PostMapping("add")
     public ResponseEntity addTeacher(@RequestBody @Valid Teacher teacher){
 
+        logger.info("add new teacher");
         teacherService.add(teacher);
         return ResponseEntity.ok("teacher added");
     }
 
     @PutMapping("update/{id}")
     public ResponseEntity updateTeacher(@PathVariable Integer id, @RequestBody @Valid Teacher teacher){
+        logger.info("update teacher");
         teacherService.update(id, teacher);
         return ResponseEntity.ok("teacher updated");
     }
@@ -44,6 +51,7 @@ public class TeacherController {
 
     @GetMapping("get-teacher/{id}")
     public ResponseEntity getTeacherById(@PathVariable Integer id){
+        logger.info("delete teacher");
         Teacher teacher = teacherService.getByID(id);
         return ResponseEntity.ok(teacher);
     }}
